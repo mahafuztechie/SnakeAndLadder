0e5cb8d9a09402d5bcf91a58b8a0e460806b5fb5
mahafuztechie <mahafuzaadeez5@gmail.com>
2022-01-17 20:15:00 +0530

    [mahafuz] adding UC3

--- SnakeLadder.cs
+++ SnakeLadder.cs
@@ -6,19 +6,36 @@ using System.Threading.Tasks;
 
 namespace SnakeAndLadder
 {
-   
-  public class Snake_Ladder
+    public class Snake_Ladder
     {
         int firstPlayerPosition;
 
         Random random = new Random();
+        int ranOption;
         public void game()
         {
             //First player starting from position 0
             firstPlayerPosition = 0;
-
             //Taking random value of Dice from 1-6
-            int diceValue = random.Next(1, 7);
+            int DiceValue = random.Next(1, 7);
+
+            ranOption = random.Next(0, 3);
+
+            if (ranOption == 0)
+            {
+                Console.WriteLine("no play");
+                firstPlayerPosition += firstPlayerPosition;
+            }
+            else if (ranOption == 1)
+            {
+                Console.WriteLine("Ladder");
+                firstPlayerPosition = firstPlayerPosition + DiceValue;
+            }
+            else
+            {
+                Console.WriteLine("Snake");
+                firstPlayerPosition = firstPlayerPosition + DiceValue;
+            }
         }
     }
 }
