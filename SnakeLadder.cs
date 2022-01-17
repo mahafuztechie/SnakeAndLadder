6ae612f1cf264f37ee66702d7609b68f48bbce2d
mahafuztechie <mahafuzaadeez5@gmail.com>
2022-01-17 19:44:15 +0530

    [mahafuz] adding UC1

---
+++ SnakeLadder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class SnakeLadder
+    {
+        int firstPlayerPosition;
+        public int game()
+        {
+          return  firstPlayerPosition = 0;
+        }
+    }
+}
