05a22308a20c63dc175c66fb7eb8d60302c301c7
mahafuztechie <mahafuzaadeez5@gmail.com>
2022-01-17 20:26:16 +0530

    [mahafuz] adding UC4

--- SnakeLadder.cs
+++ SnakeLadder.cs
@@ -6,9 +6,12 @@ using System.Threading.Tasks;
 
 namespace SnakeAndLadder
 {
+
+
     public class Snake_Ladder
     {
-        int firstPlayerPosition;
+        int firstPlayerPosition = 0;
+        int MAX_POSITION = 100;
 
         Random random = new Random();
         int ranOption;
@@ -16,26 +19,35 @@ namespace SnakeAndLadder
         {
             //First player starting from position 0
             firstPlayerPosition = 0;
-            //Taking random value of Dice from 1-6
-            int DiceValue = random.Next(1, 7);
 
-            ranOption = random.Next(0, 3);
+            //Creating while loop to reach the maximum value
+            while (firstPlayerPosition <= MAX_POSITION)
+            {
+                //Taking random value of Dice from 1-6
+                int DiceValue = random.Next(1, 7);
 
-            if (ranOption == 0)
-            {
-                Console.WriteLine("no play");
-                firstPlayerPosition += firstPlayerPosition;
-            }
-            else if (ranOption == 1)
-            {
-                Console.WriteLine("Ladder");
-                firstPlayerPosition = firstPlayerPosition + DiceValue;
-            }
-            else
-            {
-                Console.WriteLine("Snake");
-                firstPlayerPosition = firstPlayerPosition + DiceValue;
+                ranOption = random.Next(0, 3);
+
+                if (ranOption == 0)
+                {
+                    Console.WriteLine("no play");
+                    firstPlayerPosition += firstPlayerPosition;
+                }
+                else if (ranOption == 1)
+                {
+                    Console.WriteLine("Ladder");
+                    firstPlayerPosition = firstPlayerPosition + DiceValue;
+                }
+                else if (ranOption == 2)
+                {
+                    Console.WriteLine("Snake");
+                    firstPlayerPosition = firstPlayerPosition - DiceValue;
+                }
+                else
+                {
+                    Console.WriteLine("Not valid move");
+                }
             }
         }
     }
-}
+}f
